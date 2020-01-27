d6ca335eba09780fb5836024fab9335026e3efe7
kstAtBitLC <klaus.stolz@bitlc.de>
2020-01-27 13:35:33 +0100

    Add project files.

---
+++ MehrfachvererbungMitInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MehrfachvererbungMitInterfaces {
+    class Program {
+        static void Main (string [] args) {
+        }
+    }
+}
